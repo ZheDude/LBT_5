c9f81817ff10cde92dfc87cf20900e7622198824
mehmet <mehmetbalikci460@gmail.com>
2025-01-22 13:04:25 +0100

    added DNS config

--- Customer A Location 1/DC1.ps1
+++ Customer A Location 1/DC1.ps1
@@ -9,6 +9,9 @@ $SubnetMask = "255.255.255.0"
 $Gateway = "192.168.0.254"
 $InterfaceAlias = (Get-NetAdapter | Where-Object { $_.Status -eq 'Up' }).InterfaceAlias
 
+New-NetIPAddress -InterfaceAlias $InterfaceAlias -IPAddress $IP -PrefixLength 24 -DefaultGateway $Gateway
+Set-DnsClientServerAddress -InterfaceAlias $InterfaceAlias -ServerAddresses ("192.168.0.10")
+
 Install-WindowsFeature -Name AD-Domain-Services, DNS, DHCP -IncludeManagementTools
 
 # Active Directory Konfiguration
@@ -34,12 +37,31 @@ $SiteLinkName = "SiteLink-Wien-HQ-Linz-Office"
 New-ADReplicationSite -Name $SiteName
 # Erstellen der SiteLinks
 New-ADReplicationSiteLink -Name $SiteLinkName -SitesIncluded $DefaultSiteName,$SiteName -Cost 100 -ReplicationFrequencyInMinutes 15
+New-ADReplicationSubnet -Name "192.168.1.0/24" -Site $SiteName
 
-$SiteName = "St.Poelten-Office"
-$SiteLinkName = "SiteLink-Wien-HQ-St.Poelten-Office"
+$SiteName = "St-Poelten-Office"
+$SiteLinkName = "SiteLink-Wien-HQ-St-Poelten-Office"
 # Erstellen des neuen Standorts
 New-ADReplicationSite -Name $SiteName
 # Erstellen der SiteLinks
 New-ADReplicationSiteLink -Name $SiteLinkName -SitesIncluded $DefaultSiteName,$SiteName -Cost 100 -ReplicationFrequencyInMinutes 15
+New-ADReplicationSubnet -Name "172.16.0.0/24" -Site $SiteName
 
-# Konfiguration des Subnetzes für den neuen Standort
+# DNS-Konfiguration:
+$InterfaceAlias = (Get-NetAdapter | Where-Object { $_.Status -eq 'Up' }).InterfaceAlias
+Set-DnsClientServerAddress -InterfaceAlias $InterfaceAlias -ServerAddresses ("192.168.0.10", "192.168.0.11")
+
+Add-DnsServerPrimaryZone -Name "corp.murbal.at" -ReplicationScope "Forest"
+Add-DnsServerPrimaryZone -NetworkID "192.168.0.0/24" -ReplicationScope "Forest"
+Add-DnsServerPrimaryZone -NetworkID "192.168.1.0/24" -ReplicationScope "Forest"
+Add-DnsServerPrimaryZone -NetworkID "172.16.0.0/24" -ReplicationScope "Forest"
+
+Add-DnsServerResourceRecordPtr -Name "10" `
+    -ZoneName "0.168.192.in-addr.arpa" `
+    -PtrDomainName "DC1.corp.murbal.at" `
+    -ComputerName DC1.corp.5cn.at
+
+Add-DnsServerResourceRecordPtr -Name "11" `
+    -ZoneName "0.168.192.in-addr.arpa" `
+    -PtrDomainName "DC2.corp.murbal.at" `
+    -ComputerName DC1.corp.5cn.at
