10c713108cbc3d814b222f97aa7cee14813d22b7
mehmet <mehmetbalikci460@gmail.com>
2025-02-19 12:16:03 +0100

    fixed errors

--- Customer A Location 1/NPS.ps1
+++ Customer A Location 1/NPS.ps1
@@ -17,18 +17,17 @@ $DomainCreds = Get-Credential -Message "Bitte die Anmeldeinformationen eines Dom
 Add-Computer -DomainName $DomainName -Credential $DomainCreds -Restart
 
 # Konfiguration des NPS
-
 Mit netsh nps sollen nur Clients angelegt werden. die Definition der Policies erfolgt über die GUI
 netsh
 nps
 # Fortigate
-add client name "FortiGate" address = "192.168.0.254" sharedsecret = "SuperGeheim123!"
+add client name = "FortiGate" address = "192.168.0.254" sharedsecret = "SuperGeheim123!"
 add crp name = "FortiGate" processingorder = 1 conditionid = "0x100c" conditiondata = "192.168.0.254"
 # Cisco Switches
-add client name "CoreSW1" address = "192.168.0.254" sharedsecret = "SuperGeheim123!" vendor = "Cisco"
-add client name "CoreSW2" address = "192.168.0.254" sharedsecret = "SuperGeheim123!" vendor = "Cisco"
-add client name "DistSW1" address = "192.168.0.254" sharedsecret = "SuperGeheim123!" vendor = "Cisco"
-add client name "DistSw2" address = "192.168.0.254" sharedsecret = "SuperGeheim123!" vendor = "Cisco"
+add client name = "CoreSW1" address = "192.168.0.1" sharedsecret = "SuperGeheim123!" vendor = "Cisco"
+add client name = "CoreSW2" address = "192.168.0.2" sharedsecret = "SuperGeheim123!" vendor = "Cisco"
+add client name = "DistSW1" address = "192.168.0.3" sharedsecret = "SuperGeheim123!" vendor = "Cisco"
+add client name = "DistSw2" address = "192.168.0.4" sharedsecret = "SuperGeheim123!" vendor = "Cisco"
 
 
 add np name = "SwitchConnections2 (LVL15)" conditionid = "0x1fb5" conditiondata = "S-1-5-21-2862227730-4028621430-2473981021-1108" profileid = "0x1388" profiledata = "shell:priv-lvl=15" profileid = "0x100f" profiledata = "TRUE" profileid = "0x1009" profiledata = "0x1" profileid = "0x6" profiledata = "0x1" processingorder = 1
